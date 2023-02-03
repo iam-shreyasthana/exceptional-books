ce6ffe1d704085bdff8ddc07bf2e237e3188e618
Shrey Asthana <shreyasthana545@gmail.com>
2023-02-04 03:04:52 +0530

    added styling and functionalities to the buttons

--- src/App.js
+++ src/App.js
@@ -17,15 +17,70 @@ const bookDataBase = {
 
   fiction: [
     {
-      name: ""
+      name: "Harry Potter and the Half-Blood Prince",
+      rating: 5/5
+    },
+    {
+      name: "The Lord of The Rings",
+      rating: 4.5/5
+    }
+  ],
+
+  business: [
+    {
+      name: "Never Split the Difference",
+      rating: "3.5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "5/5"
     }
   ]
 };
 
 function App() {
+
+  const [selectedGenre, setGenre] = useState("fiction");
+
+  const genreOnClick = (genre) => {
+    setGenre(genre);
+  }
+
   return (
     <div className="App">
-      
+      <h1>📚Exceptional Books</h1>
+      <p className='top-heading'>
+      {" "}
+      Checkout my favorite books. Select a genre to get started
+      {" "}
+      </p>
+
+      <div>
+        {Object.keys(bookDataBase).map((genre) => (
+          <button
+            className='book-btn'
+            onClick={() => genreOnClick(genre)}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {bookDataBase[selectedGenre].map((book) => (
+            <li 
+              className="booksList"
+              key={book.name}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
